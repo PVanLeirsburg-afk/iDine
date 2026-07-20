468616d746719e1e184107458aa5c70aeee4ca9a
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-07-20 18:55:47 -0500

    Composing views to create a list row

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,18 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Pamela VanLeirsburg on 7/20/26.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemRow()
+}
