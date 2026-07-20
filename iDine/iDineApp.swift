67831be750d8db825b576bd3ac180b58893ca22c
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-07-20 18:04:14 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Pamela VanLeirsburg on 7/20/26.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
