5bbd986db83aceb052e761628d53b5e449f8ecf4
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-07-21 14:29:00 -0500

    Displaying a detail screen with NavigationLink

---
+++ iDine/ItemDetail.swift
@@ -0,0 +1,18 @@
+//
+//  ItemDetail.swift
+//  iDine
+//
+//  Created by Pamela VanLeirsburg on 7/21/26.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemDetail()
+}
