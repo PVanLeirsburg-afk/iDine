bdf47730e1650622bb9983120e6c8ff0237a92e7
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-07-21 15:57:48 -0500

    Adding items to an order with @EnvironmentObject

---
+++ iDine/OrderView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Pamela VanLeirsburg on 7/21/26.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderView()
+}
